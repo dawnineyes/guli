b0df5c688edd0f9ea12140178f0c58f2ebce395e
dawnineyes <dawnineyes@gmail.com>
2020-08-05 18:20:12 +0800

    项目初始创建

---
+++ guil-product/src/main/java/com/atguigu/guli/product/GuilProductApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilProductApplication.class, args);
+    }
+
+}
---
+++ guil-product/src/test/java/com/atguigu/guli/product/GuilProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
